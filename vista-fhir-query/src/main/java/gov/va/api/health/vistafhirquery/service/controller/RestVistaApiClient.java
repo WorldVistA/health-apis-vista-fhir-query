c48cdddcaab6c39f1ad639fe736c4e3e1213e97c
Evan Clendenning <evan.clendenning@libertyits.com>
2021-06-28 10:13:36 -0400

    Api 8090 loma linda hack (#97)

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/RestVistaApiClient.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/RestVistaApiClient.java
@@ -8,10 +8,12 @@ import gov.va.api.lighthouse.charon.api.RpcResponse;
 import gov.va.api.lighthouse.charon.api.RpcVistaTargets;
 import gov.va.api.lighthouse.charon.models.TypeSafeRpcRequest;
 import java.net.URI;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -24,6 +26,7 @@ import org.springframework.web.client.RestTemplate;
 @Builder
 @Component
 @AllArgsConstructor(onConstructor_ = @Autowired)
+@Slf4j
 public class RestVistaApiClient implements VistalinkApiClient {
   private RestTemplate restTemplate;
 
@@ -43,6 +46,20 @@ public class RestVistaApiClient implements VistalinkApiClient {
         .body(body);
   }
 
+  private Map<String, RpcPrincipal> getPrincipals(TypeSafeRpcRequest rpcRequestDetails) {
+    Map<String, RpcPrincipal> principals =
+        rpcPrincipalLookup.findByName(rpcRequestDetails.asDetails().name());
+    // Loma Linda context hack
+    RpcPrincipal maybeLomaLinda = principals.get("605");
+    if (maybeLomaLinda != null
+        && !"unset".equals(config().getLomaLindaHackContext())
+        && "VPR GET PATIENT DATA".equals(rpcRequestDetails.asDetails().name())) {
+      log.info("Performing Loma Linda context override.");
+      principals.put("605", maybeLomaLinda.contextOverride(config.getLomaLindaHackContext()));
+    }
+    return principals;
+  }
+
   /** Make a request using a full RPC Request. */
   @SneakyThrows
   public RpcResponse makeRequest(RpcRequest rpcRequest) {
@@ -59,8 +76,7 @@ public class RestVistaApiClient implements VistalinkApiClient {
     RpcRequest rpcRequest =
         RpcRequest.builder()
             .principal(RpcPrincipal.builder().accessCode("not-used").verifyCode("not-used").build())
-            .siteSpecificPrincipals(
-                rpcPrincipalLookup.findByName(rpcRequestDetails.asDetails().name()))
+            .siteSpecificPrincipals(getPrincipals(rpcRequestDetails))
             .target(target)
             .rpc(rpcRequestDetails.asDetails())
             .build();
