8fc8236013bcc697aadde0631ee11da10c64887f
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-07-06 15:00:12 -0400

    API-8163-coverage-openapi (#104)

---
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/api/R4CoverageApi.java
@@ -0,0 +1,98 @@
+package gov.va.api.health.vistafhirquery.service.api;
+
+import gov.va.api.health.r4.api.resources.Coverage;
+import gov.va.api.health.r4.api.resources.OperationOutcome;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+public interface R4CoverageApi {
+
+  @Operation(
+      summary = "Coverage Read",
+      description = "http://hl7.org/fhir/us/carin/StructureDefinition/carin-bb-coverage",
+      tags = {"Coverage"})
+  @GET
+  @Path("Coverage/{id}")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = Coverage.class)))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  Coverage coverageRead(
+      @Parameter(
+              in = ParameterIn.PATH,
+              name = "id",
+              required = true,
+              description =
+                  "The logical id of the resource. Once assigned, this value never changes.")
+          String id);
+
+  @Operation(
+      summary = "Coverage Search",
+      description = "http://hl7.org/fhir/us/carin/StructureDefinition/carin-bb-coverage",
+      tags = {"Coverage"})
+  @GET
+  @Path("Coverage")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Record found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = Coverage.Bundle.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  Coverage.Bundle coverageSearch(
+      @Parameter(hidden = true) HttpServletRequest request,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "patient",
+              description =
+                  "The patient's Integration Control Number (ICN)"
+                      + " assigned by the Master Patient Index (MPI).")
+          String icn,
+      @Parameter(
+              in = ParameterIn.QUERY,
+              name = "_count",
+              description =
+                  "The number of resources that should be returned in a single page. "
+                      + "The maximum count size is 100.")
+          @DefaultValue("30")
+          Integer count);
+}
