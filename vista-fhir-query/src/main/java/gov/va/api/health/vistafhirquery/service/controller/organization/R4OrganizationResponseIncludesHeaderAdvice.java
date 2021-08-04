58db0e885ef1f0dfc192f1480b8f8904323c188b
bryan.schofield <vasdvp.jenkins@libertyits.com>
2021-08-04 18:01:54 +0000

    Release 1.0.41 - Jenkins Build 261 (department-of-veterans-affairs/health-apis-vista-fhir-query/master)

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationResponseIncludesHeaderAdvice.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationResponseIncludesHeaderAdvice.java
@@ -3,11 +3,10 @@ package gov.va.api.health.vistafhirquery.service.controller.organization;
 import gov.va.api.health.r4.api.bundle.AbstractEntry;
 import gov.va.api.health.r4.api.resources.Organization;
 import gov.va.api.lighthouse.talos.ResponseIncludesIcnHeaderAdvice;
+import java.util.stream.Stream;
 import lombok.experimental.Delegate;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
-import java.util.stream.Stream;
 
 @ControllerAdvice
 public class R4OrganizationResponseIncludesHeaderAdvice implements ResponseBodyAdvice<Object> {
--- vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationResponseIncludesIcnHeaderAdviceTest.java
+++ vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationResponseIncludesIcnHeaderAdviceTest.java
@@ -1,5 +1,8 @@
 package gov.va.api.health.vistafhirquery.service.controller.organization;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import gov.va.api.health.r4.api.resources.Organization;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,23 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 @ExtendWith(MockitoExtension.class)
 public class R4OrganizationResponseIncludesIcnHeaderAdviceTest {
-
   @Mock R4OrganizationController controller;
 
   private MockMvc mockMvc;
-
-  @BeforeEach
-  void setUp() {
-    mockMvc =
-        MockMvcBuilders.standaloneSetup(controller)
-            .setControllerAdvice(new R4OrganizationResponseIncludesHeaderAdvice())
-            .build();
-  }
 
   @Test
   @SneakyThrows
@@ -37,5 +28,13 @@ public class R4OrganizationResponseIncludesIcnHeaderAdviceTest {
     mockMvc
         .perform(get("/r4/Organization/o1"))
         .andExpect(MockMvcResultMatchers.header().string("X-VA-INCLUDES-ICN", "NONE"));
+  }
+
+  @BeforeEach
+  void setUp() {
+    mockMvc =
+        MockMvcBuilders.standaloneSetup(controller)
+            .setControllerAdvice(new R4OrganizationResponseIncludesHeaderAdvice())
+            .build();
   }
 }
