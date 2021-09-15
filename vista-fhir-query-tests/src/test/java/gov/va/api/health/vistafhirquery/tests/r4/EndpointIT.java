0e522938aa8150c4e7063a172841d0999b5fdc1c
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2021-09-15 14:56:48 -0400

    Updated Endpoint ITs (#153)

--- vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/EndpointIT.java
+++ vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/EndpointIT.java
@@ -1,46 +1,48 @@
 package gov.va.api.health.vistafhirquery.tests.r4;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import gov.va.api.health.r4.api.resources.Endpoint;
 import gov.va.api.health.vistafhirquery.tests.SystemDefinitions;
 import gov.va.api.health.vistafhirquery.tests.TestClients;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @Slf4j
 public class EndpointIT {
   private final String apiPath = SystemDefinitions.systemDefinition().basePath().apiPath();
 
-  private final String patient = SystemDefinitions.systemDefinition().publicIds().patient();
+  static Predicate<Integer> empty() {
+    return size -> size == 0;
+  }
+
+  static Predicate<Integer> notEmpty() {
+    return empty().negate();
+  }
+
+  static Stream<Arguments> search() {
+    final String patient = SystemDefinitions.systemDefinition().publicIds().patient();
+    return Stream.of(
+        arguments("r4/Endpoint", notEmpty()),
+        arguments("r4/Endpoint?status=active", notEmpty()),
+        arguments("r4/Endpoint?status=active&patient=" + patient, notEmpty()),
+        arguments("r4/Endpoint?patient=" + patient, notEmpty()),
+        arguments("r4/Endpoint?status=ANYTHING_NOT_ACTIVE", empty()),
+        arguments("r4/Endpoint?status=active&patient=" + patient, notEmpty()));
+  }
 
   @ParameterizedTest
-  @ValueSource(strings = {"r4/Endpoint", "r4/Endpoint?status=active"})
-  void search(String query) {
+  @MethodSource
+  void search(String query, Predicate<Integer> bundleSizeCondition) {
     var requestPath = apiPath + query;
     log.info("Verify {} is Bundle (200)", requestPath);
     var bundle =
         TestClients.basePath().get(requestPath).expect(200).expectValid(Endpoint.Bundle.class);
-    assertThat(bundle.total()).isGreaterThan(0);
-  }
-
-  @Test
-  void searchByKnownPatient() {
-    var requestPath = "r4/Endpoint?patient=" + patient;
-    log.info("Verify {} is Bundle (200)", requestPath);
-    var bundle =
-        TestClients.basePath().get(requestPath).expect(200).expectValid(Endpoint.Bundle.class);
-    assertThat(bundle.total()).isGreaterThan(0);
-  }
-
-  @Test
-  void searchWithBadStatus() {
-    var requestPath = apiPath + "r4/Endpoint?status=INVALID";
-    log.info("Verify {} is Bundle (200)", requestPath);
-    var bundle =
-        TestClients.basePath().get(requestPath).expect(200).expectValid(Endpoint.Bundle.class);
-    assertThat(bundle.total()).isEqualTo(0);
+    assertThat(bundle.total()).matches(bundleSizeCondition);
   }
 }
