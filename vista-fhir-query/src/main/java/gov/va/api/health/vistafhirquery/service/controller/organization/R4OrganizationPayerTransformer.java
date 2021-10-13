61a0652b9d22ec282451fa2624d5e7d3ad4366c1
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-10-13 15:39:17 -0400

    Deactive DEACTIVATED (#221)

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationPayerTransformer.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/organization/R4OrganizationPayerTransformer.java
@@ -25,9 +25,9 @@ import lombok.Value;
 @Builder
 public class R4OrganizationPayerTransformer {
   /** The fields needed by the transformer. */
+  // ToDo Payer.DEACTIVATED, -- https://vajira.max.gov/browse/API-10580
   public static final List<String> REQUIRED_FIELDS =
-      List.of(
-          Payer.DEACTIVATED, Payer.EDI_ID_NUMBER_INST, Payer.EDI_ID_NUMBER_PROF, Payer.PAYER_NAME);
+      List.of(Payer.EDI_ID_NUMBER_INST, Payer.EDI_ID_NUMBER_PROF, Payer.PAYER_NAME);
 
   @NonNull String site;
 
