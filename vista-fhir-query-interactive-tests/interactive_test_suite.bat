e0cfaa4369ad5b664f4db07aefb12c834713b78c
ilaflamme-va <43884028+ilaflamme-va@users.noreply.github.com>
2021-12-08 10:37:12 -0500

    interactive-tests-windows-.bat-file (#320)

---
+++ vista-fhir-query-interactive-tests/interactive_test_suite.bat
@@ -0,0 +1,9 @@
+@echo off
+echo "Welcome to the health-apis interactive test suite!"
+echo "Pulling docker image..."
+set dockerImage=vasdvp/health-apis-vista-fhir-query-interactive-tests:latest
+docker pull %dockerImage%
+set scriptDir=%~dp0
+set scriptDir=%scriptDir:~0,-1%
+docker run --rm -it -v "%scriptDir%\test_properties:/sentinel/test_properties" %dockerImage%
+cmd
