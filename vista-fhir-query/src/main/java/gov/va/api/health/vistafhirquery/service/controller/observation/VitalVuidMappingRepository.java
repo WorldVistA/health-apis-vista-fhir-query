0071e113df2a020eb441d40a86f836b1b547e6f9
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2022-02-10 08:33:53 -0500

    transaction isolation levels will now use database defaults (#395)

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/observation/VitalVuidMappingRepository.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/observation/VitalVuidMappingRepository.java
@@ -4,12 +4,9 @@ import gov.va.api.health.autoconfig.logging.Loggable;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 /** Repository for interacting with the VitalVuidMapping table. */
 @Loggable
-@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface VitalVuidMappingRepository
     extends JpaSpecificationExecutor<VitalVuidMappingEntity>,
         PagingAndSortingRepository<VitalVuidMappingEntity, String> {
