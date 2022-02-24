809a14921b338da46ba730672a2e44074a4b87aa
jabando-va <42628485+jabando-va@users.noreply.github.com>
2022-02-24 14:06:37 -0500

    controller spring annotations (#427)

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/coverage/R4SiteInsuranceBufferCoverageController.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/coverage/R4SiteInsuranceBufferCoverageController.java
@@ -34,6 +34,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -63,7 +64,9 @@ public class R4SiteInsuranceBufferCoverageController {
       value = "/hcs/{site}/r4/Coverage",
       consumes = {"application/json", "application/fhir+json"})
   public void coverageCreate(
-      @Redact HttpServletResponse response, String site, @Redact Coverage body) {
+      @Redact HttpServletResponse response,
+      @PathVariable(value = "site") String site,
+      @Redact @RequestBody Coverage body) {
     var ctx =
         updateOrCreate(
             CreatePatientRecordWriteContext.<Coverage>builder()
